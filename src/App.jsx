ebe95cab7c2896367c938a9ef8c2ef9bdd12a366
salma hamdi <saalmahm51@gmail.com>
2026-02-01 16:51:34 +0100

    SF-8: restaurer la session au chargement avec fetchSession

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchSession } from './store/authSlice';
 import LoginPage from './pages/LoginPage';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Tentative de restauration de session (CLIENT)
+    dispatch(fetchSession());
+  }, [dispatch]);
+
   return (
     <BrowserRouter>
       <Routes>
